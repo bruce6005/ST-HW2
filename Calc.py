cfaf104259fe2aa3a36cb58177245858d03571d4
bruce6005 <bruce6005@gmail.com>
2025-04-08 11:23:28 +0800

    CI file upload

--- Calc.py
+++ Calc.py
@@ -5,6 +5,6 @@ class Calculator:
         return a - b
     def multiply(self, a, b):
         return a * b    
-    def divide(self, a, b):
-        return a / b
+    # def divide(self, a, b):
+    #     return a / b
 
--- test_Calc.py
+++ test_Calc.py
@@ -4,10 +4,10 @@ def test_subtract():
     calc = Calculator()
     result = calc.subtract(5, 2)
     assert result == 3
-# def test_multiply():
-#     calc = Calculator()
-#     result = calc.multiply(3, 4)
-#     assert result == 12
+def test_multiply():
+    calc = Calculator()
+    result = calc.multiply(3, 4)
+    assert result == 12
 def test_divide():
     calc = Calculator()
     result = calc.divide(10, 4)
